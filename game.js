44f1a1ee5553ed7efb17ea896092158420d295ef
Borimir1 <bobi_aleksiev46@abv.bg>
2023-02-14 16:21:01 +0200

    letsgo

--- game.js
+++ game.js
@@ -1,5 +1,19 @@
 let canvas=document.getElementById("gameCanvas");
-let context=canvas.getContent("2d");
-canvas.width=300;
-canvas.height=200;
-context.strokeRect(40,40,50,50);
+let context=canvas.getContext("2d");
+let nx=4;
+let ny=3;
+let sqside=50;
+let herox=2;
+let heroy=1;
+canvas.width=nx*sqside;
+canvas.height=ny*sqside;
+for(let i=0;i<nx;i++) {
+	for(let j=0;j<ny;j++) {
+		context.strokeRect(i*sqside,j*sqside,sqside,sqside);
+	}
+}
+//context.fillRect(herox*sqside,heroy*sqside,sqside,sqside);
+let heroimg=new Image();
+heroimg.src="hero.png"
+context.drawImage(heroimg,herox*sqside,heroy*sqside,sqside,sqside);
+
