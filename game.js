2871bffe2a303a0d6b973e67a2539dd1d46dd4f4
Borimir1 <bobi_aleksiev46@abv.bg>
2023-02-17 12:14:18 +0200

    raaah

--- game.js
+++ game.js
@@ -5,15 +5,26 @@ let ny=3;
 let sqside=50;
 let herox=2;
 let heroy=1;
-canvas.width=nx*sqside;
-canvas.height=ny*sqside;
-for(let i=0;i<nx;i++) {
-	for(let j=0;j<ny;j++) {
-		context.strokeRect(i*sqside,j*sqside,sqside,sqside);
-	}
-}
-//context.fillRect(herox*sqside,heroy*sqside,sqside,sqside);
 let heroimg=new Image();
 heroimg.src="hero.png"
-context.drawImage(heroimg,herox*sqside,heroy*sqside,sqside,sqside);
+canvas.width=nx*sqside;
+canvas.height=ny*sqside;
 
+function drawMap() {
+	context.clearRect(0,0,canvas.width,canvas.height);
+	context.drawImage(heroimg,herox*sqside,heroy*sqside,sqside,sqside);
+	for(let i=0;i<nx;i++) {
+		for(let j=0;j<ny;j++) {
+		context.strokeRect(i*sqside,j*sqside,sqside,sqside);
+		}
+	}
+}
+drawMap();
+canvas.onclick=function(e){
+	let x=e.x-canvas.offsetLeft;
+	let y=e.y-canvas.offsetTop;
+	herox=Math.floor(x/sqside);
+	heroy=Math.floor(y/sqside);
+	console.log(herox+" "+heroy);
+	drawMap();
+}
