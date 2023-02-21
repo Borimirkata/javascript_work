a6da590dd81a0667b6a19af548f3b34d5086367a
Borimir1 <bobi_aleksiev46@abv.bg>
2023-02-21 16:20:00 +0200

    ohio

--- game.js
+++ game.js
@@ -5,11 +5,21 @@ let ny=8;
 let sqside=50;
 let herox=0;
 let heroy=0;
+let gol = document.getElementById("gol");
+let result=0;
+let coinx=Math.floor(Math.random()*7)+1;
+let coiny=Math.floor(Math.random()*7)+1;
+let bomb1x=Math.floor(Math.random()*7)+1;
+let bomb1y=Math.floor(Math.random()*7)+1;
+let bomb2x=Math.floor(Math.random()*7)+1;
+let bomb2y=Math.floor(Math.random()*7)+1;
+let bomb3x=Math.floor(Math.random()*7)+1;
+let bomb3y=Math.floor(Math.random()*7)+1;
 let heroimg=new Image();
 heroimg.src="hero.png"
 canvas.width=nx*sqside;
 canvas.height=ny*sqside;
-
+document.getElementById("gol").value=result;
 function drawMap() {
 	context.clearRect(0,0,canvas.width,canvas.height);
 	context.drawImage(heroimg,herox*sqside,heroy*sqside,sqside,sqside);
@@ -20,10 +30,44 @@ function drawMap() {
 				context.fillStyle="brown";
 				context.fillRect(i*sqside,j*sqside,sqside,sqside);
 			}
+			if(coinx==i && coiny==j){
+				context.fillStyle="yellow";
+				context.fillRect(i*sqside,j*sqside,sqside,sqside);
+			}
+			if(herox==coinx && heroy==coiny){
+				result++;
+				document.getElementById("gol").value=result;
+				coinx=Math.floor(Math.random()*7)+1;
+				coiny=Math.floor(Math.random()*7)+1;
+			}
+			if(bomb1x==i && bomb1y==j && bomb1x!=coinx && bomb1y!=coiny){
+				context.fillStyle="brown";
+				context.fillRect(i*sqside,j*sqside,sqside,sqside);
+			}
+			if(bomb2x==i && bomb2y==j && bomb2x!=coinx && bomb2y!=coiny){
+				context.fillStyle="brown";
+				context.fillRect(i*sqside,j*sqside,sqside,sqside);
+			}
+			if(bomb3x==i && bomb3y==j && bomb3x!=coinx && bomb3y!=coiny){
+				context.fillStyle="brown";
+				context.fillRect(i*sqside,j*sqside,sqside,sqside);
+			}
+			if((herox==bomb1x && heroy==bomb1y) || (herox==bomb2x && heroy==bomb2y) || (herox==bomb3x && heroy==bomb3y)){
+				window.location.reload();
+			}
+			
+			//if(coinx==herox && coiny==heroy){
+				//coinx=Math.floor(Math.random()*7)+1;
+				//coiny=Math.floor(Math.random()*7)+1;
+				//context.fillStyle="yellow";
+				//context.fillRect(i*sqside,j*sqside,sqside,sqside);
+			//{
 		}
 	}
 	context.drawImage(heroimg,herox*sqside,heroy*sqside,sqside,sqside);
 }
+
+
 drawMap();
 //canvas.onclick=function(e){
 	//let x=e.x-canvas.offsetLeft;
